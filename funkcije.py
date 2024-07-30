bac9f1031a117d46033fac05580ae489b54cf9f7
Matej Knap <mk54674@student.uni-lj.si>
2024-07-30 11:21:40 +0200

    Dodal pobiranje naslovov in id-jev Program iz datoteke godot.html najde podatke in jih shrani v dateteko godot.csv

--- funkcije.py
+++ funkcije.py
@@ -1,7 +1,20 @@
 import csv
 import os
 import requests
+import re
 
 
 def download_url_to_string(url):
     return requests.get(url)
+
+
+def findall_godot_games(html):
+    vzorec = r'<a target="_blank" class="b" href="/app/(\d*)/">(.*)</a>'
+    return [(x.group(1), x.group(2)) for x in re.finditer(vzorec, html)]
+
+
+def naredi_godot_csv(datoteka, prva_vrstica, seznam):
+    with open(datoteka, "w", encoding="utf8") as file:
+        file.write(prva_vrstica + "\n")
+        for igra in seznam:
+            file.write(str(igra[0]) + "," + igra[1] + "\n")
--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import funkcije
 
 url = "https://steamdb.info/charts"
+
+with open("godot.html", "r", encoding="utf8") as file:
+    godot_html = file.read()
+
+godot_seznam = funkcije.findall_godot_games(godot_html)
+
+funkcije.naredi_godot_csv("godot.csv", "id,naslov", godot_seznam)
