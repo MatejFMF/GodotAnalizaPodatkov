26f459a380758334acac067d8f65ed6535dbc216
Matej Knap <mk54674@student.uni-lj.si>
2024-08-01 19:54:32 +0200

    main.py v godot.csv shrani vse pridobljene podatke

--- funkcije.py
+++ funkcije.py
@@ -5,12 +5,12 @@ import re
 
 
 def download_url_to_string(url):
-    return requests.get(url)
+    return requests.get(url).text
 
 
 def findall_godot_games(html):
     vzorec = r'<a target="_blank" class="b" href="/app/(\d*)/">(.*)</a>'
-    return [(x.group(1), x.group(2)) for x in re.finditer(vzorec, html)]
+    return ((x.group(1), x.group(2)) for x in re.finditer(vzorec, html))
 
 
 def naredi_godot_csv(datoteka, prva_vrstica, seznam):
@@ -21,38 +21,80 @@ def naredi_godot_csv(datoteka, prva_vrstica, seznam):
 
 
 class Igra:
-    0
 
     def __init__(self) -> None:
         pass
 
     def __str__(self) -> str:
-        return str((self.ime, "id: " + self.id, self.release_date))
+        return str((self.ime, "id: " + self.id))
 
 
 def get_game_info(html):
     nova_igra = Igra()
     ime_vzorec = r'<div id="appHubAppName" class="apphub_AppName">(.*)</div>'
-    nova_igra.ime = re.search(ime_vzorec, html).group(1)
+    try:
+        nova_igra.ime = re.search(ime_vzorec, html).group(1)
+    except:
+        nova_igra.ime = "None"
     id_link_vzorec = (
         r'"og:url" content="(https://store.steampowered.com/app/(\d+).*)/">'
     )
-    nova_igra.link = re.search(id_link_vzorec, html).group(1)
-    nova_igra.id = re.search(id_link_vzorec, html).group(2)
-    description_vzorec = r'"og:description" content="(.*)">'
-    nova_igra.description = re.search(description_vzorec, html).group(1)
+    try:
+        nova_igra.link = re.search(id_link_vzorec, html).group(1)
+    except:
+        nova_igra.link = "None"
+    try:
+        nova_igra.id = re.search(id_link_vzorec, html).group(2)
+    except:
+        nova_igra.id = "None"
     developer_vzorec = r'<div class="summary column" id="developers_list">\n?\s*<a href="https://store.steampowered.com/.*">(.*)</a>'
-    nova_igra.developer = re.search(developer_vzorec, html).group(1)
+    try:
+        nova_igra.developer = re.search(developer_vzorec, html).group(1)
+    except:
+        nova_igra.vzorec = "None"
     publisher_vzorec = r'<div class="subtitle column">Publisher:</div>\n?\s*<div class="summary column">\n?\s*<a href="https://store.steampowered.com/.*">(.*)</a>'
-    nova_igra.publisher = re.search(publisher_vzorec, html).group(1)
+    try:
+        nova_igra.publisher = re.search(publisher_vzorec, html).group(1)
+    except:
+        nova_igra.publisher = "None"
     release_date_vzorec = r"<b>Release Date:</b> (.*)<br>"
-    nova_igra.release_date = re.search(release_date_vzorec, html).group(1)
-    recent_reviews_vzorec = r'<div class="subtitle column">Recent Reviews:</div>\n?\s*<div class="summary column">\n?\s*<span class=".*">(.*)</span>\n?\s*<span class="responsive_hidden">\n?\s*\((.+)\)\n?\s*</span>\n\s*<span class="nonresponsive_hidden responsive_reviewdesc">\n\s*.*(\d+%)'
-    nova_igra.recent_reviews = re.search(recent_reviews_vzorec, html).group(1, 2, 3)
+    try:
+        nova_igra.release_date = re.search(release_date_vzorec, html).group(1)
+    except:
+        nova_igra.vzorec = "None"
+    recent_reviews_vzorec = r'<div class="subtitle column">Recent Reviews:</div>\n?\s*<div class="summary column">\n?\s*<span class=.*>(.*)</span>\n?\s*<span class="responsive_hidden">\n?\s*\((.+)\)\n?\s*</span>\n?\s*<span class="nonresponsive_hidden responsive_reviewdesc">\n\s*- (\d+%)'
+    try:
+        nova_igra.recent_reviews = str(
+            re.search(recent_reviews_vzorec, html).group(1, 2, 3)
+        )
+    except:
+        nova_igra.recent_reviews = "None"
     all_reviews_vzorec = r'<div class="subtitle column all">All Reviews:</div>\n?\s*<div class="summary column">\n?\s*<span class=.*>(.*)</span>\n?\s*<span class="responsive_hidden">\n?\s*\((.*)\)\n?\s*</span>\n?\s*<span class="nonresponsive_hidden responsive_reviewdesc">\n?\s*- (\d+%)'
-    nova_igra.all_reviews = re.search(all_reviews_vzorec, html).group(1, 2, 3)
+    try:
+        nova_igra.all_reviews = str(re.search(all_reviews_vzorec, html).group(1, 2, 3))
+    except:
+        nova_igra.all_reviews = "None"
     genre_vzorec = r'<a href="https://store.steampowered.com/genre/.*?">(.*?)</a>'
-    nova_igra.genre = tuple(x for x in re.findall(genre_vzorec, html))
+    try:
+        nova_igra.genre = str(tuple(x for x in re.findall(genre_vzorec, html)))
+    except:
+        nova_igra.genre = "None"
     achievements_vzorec = r'<div class="responsive_banner_link_title responsive_chevron_right">View Steam Achievements <span class="responsive_banner_link_total">\((\d*)\)</span></div>'
-    nova_igra.achievements = re.search(achievements_vzorec, html).group(1)
+    try:
+        nova_igra.achievements = re.search(achievements_vzorec, html).group(1)
+    except:
+        nova_igra.achievements = "0"
+    description_vzorec = r'"og:description" content="(.*)">'
+    try:
+        nova_igra.description = re.search(description_vzorec, html).group(1)
+    except:
+        nova_igra.description = "None"
     return nova_igra
+
+
+def id_to_link(id):
+    return "https://store.steampowered.com/app/" + str(id)
+
+
+def game_info_to_string(igra: Igra):
+    return f"{igra.id};{igra.ime};{igra.link};{igra.release_date};{igra.developer};{igra.publisher};{igra.recent_reviews};{igra.all_reviews};{igra.genre};{igra.achievements};{igra.description}"
--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import funkcije
 
-url = "https://steamdb.info/charts"
+steam_url = "https://store.steampowered.com/search/?ignore_preferences=1"
 
 with open("godot.html", "r", encoding="utf8") as file:
     godot_html = file.read()
 
 godot_seznam = funkcije.findall_godot_games(godot_html)
 
-funkcije.naredi_godot_csv("godot.csv", "id,naslov", godot_seznam)
+
+# funkcije.naredi_godot_csv("godot.csv", "id,naslov", godot_seznam)
+
+with open("godot.csv", "w", encoding="utf8") as file:
+    file.write(
+        "id;ime;link;release_date;developer;publisher;recent_reviews;all_reviews;genre;achievements;description"
+        + "\n"
+    )
+    for igra in godot_seznam:
+        url: str = funkcije.id_to_link(igra[0])
+        html: str = funkcije.download_url_to_string(url)
+        game_info = funkcije.get_game_info(html)
+        print(game_info)
+        print(game_info.recent_reviews)
+        print(game_info.all_reviews)
+        file.write(funkcije.game_info_to_string(game_info) + "\n")
