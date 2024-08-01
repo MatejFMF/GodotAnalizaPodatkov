fa3e2ca793a5488983b8d4d587c9d93434b04b08
Matej Knap <mk54674@student.uni-lj.si>
2024-08-01 13:01:58 +0200

    Dodal  funkcijo, ki iz html-ja igre pridobi podatke

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # GodotAnalizaPodatkov
-Projektna naloga, v kateri bom iz podatkovne baze pridobil in analiziral podatke iger sprogramiranih v Godotu.
+Projektna naloga, v kateri bom iz podatkovne baze pridobil in analiziral podatke iger sprogramiranih v Godotu objavljene na portal Steam.
 
-Podatke bom pridobil iz spletne strani https://steamdb.info/tech/Engine/Godot/
+Seznam iger bom pridobil iz spletne strani https://steamdb.info/tech/Engine/Godot/,
+podatke posamičnih iger pa bom pridobil iz spletne strani https://store.steampowered.com/.
--- funkcije.py
+++ funkcije.py
@@ -18,3 +18,31 @@ def naredi_godot_csv(datoteka, prva_vrstica, seznam):
         file.write(prva_vrstica + "\n")
         for igra in seznam:
             file.write(str(igra[0]) + "," + igra[1] + "\n")
+
+
+class Igra:
+    0
+
+    def __init__(self) -> None:
+        pass
+
+    def __str__(self) -> str:
+        return str((self.ime, "id: " + self.id))
+
+
+def get_game_info(html):
+    nova_igra = Igra()
+    ime_vzorec = r'<div id="appHubAppName" class="apphub_AppName">(.*)</div>'
+    nova_igra.ime = re.search(ime_vzorec, html).group(1)
+    id_link_vzorec = (
+        r'"og:url" content="(https://store.steampowered.com/app/(\d+).*)/">'
+    )
+    nova_igra.link = re.search(id_link_vzorec, html).group(1)
+    nova_igra.id = re.search(id_link_vzorec, html).group(2)
+    description_vzorec = r'"og:description" content="(.*)">'
+    nova_igra.description = re.search(description_vzorec, html).group(1)
+    developer_vzorec = r'<div class="summary column" id="developers_list">\s*<a href="https://store.steampowered.com/curator/.*">(.*)</a>'
+    nova_igra.developer = re.search(developer_vzorec, html).group(1)
+    publisher_vzorec = r'<div class="subtitle column">Publisher:</div>\s*<div class="summary column">\s*<a href="https://store.steampowered.com/curator/.*">(.*)</a>'
+    nova_igra.publisher = re.search(publisher_vzorec, html).group(1)
+    return nova_igra
