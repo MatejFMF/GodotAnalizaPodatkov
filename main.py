9e13bc24484087b9bac050d2808536b7d02462d9
Matej Knap <mk54674@student.uni-lj.si>
2024-08-06 18:13:32 +0200

    Zamenjal main.py za main_hitri.py

--- main.py
+++ main.py
@@ -1,27 +1,65 @@
 import funkcije
-
-
-steam_url = "https://store.steampowered.com/search/?ignore_preferences=1"
+import aiohttp
+import asyncio
 
 with open("godot.html", "r", encoding="utf8") as file:
     godot_html = file.read()
 
 godot_seznam = funkcije.findall_godot_games_ids(godot_html)
 
-# cca. 25 minut
-with open("godot.csv", "w", encoding="utf8") as file:
-    file.write(
-        "id;ime;link;app_type;cena;discount;release_date;developer;publisher;all_reviews;genre;achievements;description"
-        + "\n"
-    )
-    stevilo_iger = len(godot_seznam)
-    x = 1
-    for id in godot_seznam:
-        url: str = funkcije.id_to_link(id)
-        html: str = funkcije.download_url_to_string(url)
-        game_info = funkcije.get_game_info(html)
-        print(game_info, f"-- {x}/{stevilo_iger} --")
-        file.write(funkcije.game_info_to_string(game_info) + "\n")
-        x += 1
+urls = [funkcije.id_to_link(id) for id in godot_seznam]
 
-    print("Task complete.")
+
+# S tema spremenljivkama nadzoriraš, kako hitro pridobivaš podatke
+# Če je prehirto, lahko spletna stran zavrne
+# cca. 5 minut
+request_time = 1
+no_of_requests = 20
+
+
+###
+
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
+
+async def main():
+    with open("godot.csv", "w", encoding="utf8") as file:
+        file.write(
+            "id;ime;link;app_type;cena;discount;release_date;developer;publisher;all_reviews;genre;achievements;description"
+            + "\n"
+        )
+        counter = 0
+        stevilo_iger = len(urls)
+        tasks: list = []
+        async with aiohttp.ClientSession() as session:
+            for url in urls:
+                tasks.append(fetch(session, url))
+                counter += 1
+                if counter % no_of_requests == 0:
+                    htmls = await asyncio.gather(*tasks)
+                    for html in htmls:
+                        igra = funkcije.get_game_info(html)
+                        file.write(funkcije.game_info_to_string(igra) + "\n")
+                        print(igra)
+                    print(f"-- {counter}/{stevilo_iger} --")
+                    tasks.clear()
+                    await asyncio.sleep(request_time)
+            htmls = await asyncio.gather(*tasks)
+            for html in htmls:
+                igra = funkcije.get_game_info(html)
+                file.write(funkcije.game_info_to_string(igra) + "\n")
+                print(igra)
+            print(f"-- {counter}/{stevilo_iger} --")
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+
+# Ta del kode je deloma prekopiran iz https://stackoverflow.com/a/50312981
+
+
+print("-- Task complete --")
